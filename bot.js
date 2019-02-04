2452ec86488eaddea6e1aadf1eea73b5bbfd7fe3
Xenoyia <ccplukeg@gmail.com>
2019-02-04 18:32:27 +0000

    Update

--- bot.js
+++ bot.js
@@ -1,6 +1,16 @@
+// --------------------------------
+// Important requires and constants
+// --------------------------------
+
 const { CommandoClient } = require('discord.js-commando');
 const path = require('path');
+const Discord = require('discord.js');
+const Utils = require("./core/Utils.js");
 const config = require("./config.json");
+
+// --------------------------------
+// Register the client and commands
+// --------------------------------
 
 const client = new CommandoClient({
     commandPrefix: '!',
@@ -21,9 +31,42 @@ client.registry
     .registerDefaultCommands()
     .registerCommandsIn(path.join(__dirname, 'commands'));
 
+// -------------------------------
+// Set up the new day announcement
+// -------------------------------
+
+function newDay() {
+	var serverStartTime = new Date("2018-06-15").getTime();
+	var midnight = new Date();
+	midnight.setHours( 24 );
+	midnight.setMinutes( 0 );
+	midnight.setSeconds( 0 );
+	midnight.setMilliseconds( 0 );
+	var timeUntilMidnight =  ( midnight.getTime() - new Date().getTime() );
+	console.log("New Day Check: "+timeUntilMidnight+"ms until midnight!");
+	setTimeout(() => {
+			const newDayMsg = new Discord.RichEmbed()
+				.setAuthor("House of Dragons", "https://i.imgur.com/CyAb3mV.png")
+				.setThumbnail("https://i.imgur.com/CfosMZR.png")
+				.addField("New Day Alert", 
+					"It's midnight in GMT and so begins a new day!\n\n"+
+					"It has been **"+Utils.formatTimeSince(serverStartTime, true)+"** since the House of Dragons began!\n\n"+
+					"The purge is commencing! Any guests who have not accepted the rules will now be kicked!\n\n"+
+					"As of now, there are **"+client.users.size+"** members of the House of Dragons, including bots!")
+			client.channels.get('541042784396640257').send(newDayMsg);
+			// do kicking stuff here, not implemented
+			newDay();
+		},timeUntilMidnight); // Check this every second
+}
+
+// -------------------------------
+// Let's a-go!
+// -------------------------------
+	
 client.on('ready', () => {
-    console.log('Logged in!');
-    client.user.setActivity('game');
+	console.log("Bot has started, with "+client.users.size+" users!"); 
+	client.user.setActivity("with dragon butts");
+	newDay();
 });
 
 client.login(config.token);
---
+++ commands/pet/release.js
@@ -0,0 +1,71 @@
+const {
+    Command
+} = require('discord.js-commando');
+const Utils = require('../../core/utils.js');
+const Discord = require('discord.js');
+
+module.exports = class ReleaseCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'release',
+            group: 'pet',
+            memberName: 'release',
+            description: 'Release your active pet',
+            examples: ['release'],
+            args: [
+			{
+                key: 'verify',
+                prompt: 'Verify the release?',
+                type: 'string',
+				default: ''
+			}]
+        });
+    }
+
+    async run(msg, {verify}) {
+		const embedMsg = new Discord.RichEmbed()
+			.setAuthor("House of Dragons Notice", "https://i.imgur.com/CyAb3mV.png")
+			.setColor("#FDF018")
+		
+        let queryRes = await Utils.queryDB("SELECT * FROM users WHERE discordID=" + msg.author.id);
+		var userID = queryRes[0].id;
+		var pets = JSON.parse(queryRes[0].petID);
+		var active = queryRes[0].activePet;
+		if(active > 0) {
+			let petRes = await Utils.queryDB("SELECT * FROM pets WHERE id="+active);
+			let petName = petRes[0].name;
+			let petLevel = petRes[0].level;
+			if(verify !== "verify") {
+				embedMsg.addField("Are you sure?", "If you're sure you want to release your pet **"+petName+"** (Affection Level **"+petLevel+"**), type `!release verify` now!");
+				return msg.embed(embedMsg);
+			} else {
+				embedMsg.addField("Pet Released", "Your pet has been released!");
+				
+				var newPets = new Array();
+				
+				for(var pi = 0; pi < pets.length; pi++) {
+					if(pets[pi] == active) {
+						console.log("DB: Found active pet!");
+					} else {
+						newPets.push(pets[pi]);
+					}
+				}
+				
+				if(pets.length > 1) {
+					console.log("DB: Has other pets");
+					await Utils.queryDB("UPDATE users SET activePet="+newPets[0]+", petID='"+JSON.stringify(newPets)+"' WHERE discordID="+msg.author.id);
+				} else {
+					console.log("DB: Has no other pets");
+					await Utils.queryDB("UPDATE users SET activePet=0, petID='[]' WHERE discordID="+msg.author.id);
+				}
+				console.log("DB: Deleting pet ID "+active);
+				await Utils.queryDB("INSERT pets_graveyard SELECT * FROM pets WHERE id="+active);
+				await Utils.queryDB("DELETE FROM pets WHERE id="+active);
+				return msg.embed(embedMsg);
+			}
+		} else {
+			embedMsg.addField("Can't Release Pet", "You don't have a pet to release, get one with `hod?adopt`!");
+			return msg.embed(embedMsg);
+		}
+	};
+}
