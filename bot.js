e548eedf592e4ba36b1428095f36b05ec067cb01
Xenoyia <ccplukeg@gmail.com>
2019-02-04 19:36:22 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 // --------------------------------
 // Important requires and constants
 // --------------------------------
-
-const { CommandoClient } = require('discord.js-commando');
+const {
+    CommandoClient
+} = require('discord.js-commando');
 const path = require('path');
 const Discord = require('discord.js');
 const Utils = require("./core/Utils.js");
@@ -35,28 +36,27 @@ client.registry
 // Set up the new day announcement
 // -------------------------------
 
-function newDay() {
-	var serverStartTime = new Date("2018-06-15").getTime();
-	var midnight = new Date();
-	midnight.setHours( 24 );
-	midnight.setMinutes( 0 );
-	midnight.setSeconds( 0 );
-	midnight.setMilliseconds( 0 );
-	var timeUntilMidnight =  ( midnight.getTime() - new Date().getTime() );
-	console.log("New Day Check: "+timeUntilMidnight+"ms until midnight!");
-	setTimeout(() => {
-			const newDayMsg = new Discord.RichEmbed()
-				.setAuthor("House of Dragons", "https://i.imgur.com/CyAb3mV.png")
-				.setThumbnail("https://i.imgur.com/CfosMZR.png")
-				.addField("New Day Alert", 
-					"It's midnight in GMT and so begins a new day!\n\n"+
-					"It has been **"+Utils.formatTimeSince(serverStartTime, true)+"** since the House of Dragons began!\n\n"+
-					"The purge is commencing! Any guests who have not accepted the rules will now be kicked!\n\n"+
-					"As of now, there are **"+client.users.size+"** members of the House of Dragons, including bots!")
-			client.channels.get('541042784396640257').send(newDayMsg);
-			// do kicking stuff here, not implemented
-			newDay();
-		},timeUntilMidnight);
+async function newDay() {
+    var serverStartTime = new Date("2018-06-15").getTime();
+    var midnight = new Date();
+    midnight.setHours(24);
+    midnight.setMinutes(0);
+    midnight.setSeconds(0);
+    midnight.setMilliseconds(0);
+    var timeUntilMidnight = (midnight.getTime() - new Date().getTime());
+    console.log("New Day Check: " + timeUntilMidnight + "ms until midnight!");
+	await Utils.delay(timeUntilMidnight);
+    const newDayMsg = new Discord.RichEmbed()
+		.setAuthor("House of Dragons", "https://i.imgur.com/CyAb3mV.png")
+		.setThumbnail("https://i.imgur.com/CfosMZR.png")
+		.addField("New Day Alert",
+			"It's midnight in GMT and so begins a new day!\n\n" +
+			"It has been **" + Utils.formatTimeSince(serverStartTime, true) + "** since the House of Dragons began!\n\n" +
+			"The purge is commencing! Any guests who have not accepted the rules will now be kicked!\n\n" +
+			"As of now, there are **" + client.users.size + "** members of the House of Dragons, including bots!")
+	await kickGuests();
+    client.channels.get('541042784396640257').send(newDayMsg);
+    newDay();
 }
 
 // ----------------------------
@@ -64,60 +64,60 @@ function newDay() {
 // ----------------------------
 
 async function petUpdates() {
-		await Utils.delay(10000);
-        let petSQL = await Utils.queryDB("SELECT * FROM pets");
+    await Utils.delay(10000);
+    let petSQL = await Utils.queryDB("SELECT * FROM pets");
 
-        if (petSQL.length > 0) {
-            var currentTime = new Date().getTime();
-            for (var i = 0; i < petSQL.length; i++) {
-                if (petSQL[i].isSleeping == 1) {
-                    var sleepTime = petSQL[i].sleepTime;
-                    if (currentTime > sleepTime) {
-                        await Utils.queryDB("UPDATE pets SET isSleeping = 0 WHERE id=" + petSQL[i].id);
-                        console.log("DB: Waking up pet");
-                        client.users.get(petSQL[i].ownerName).send("Your active pet **"+petSQL[i].name+"** has woken up!");
-                    }
+    if (petSQL.length > 0) {
+        var currentTime = new Date().getTime();
+        for (var i = 0; i < petSQL.length; i++) {
+            if (petSQL[i].isSleeping == 1) {
+                var sleepTime = petSQL[i].sleepTime;
+                if (currentTime > sleepTime) {
+                    await Utils.queryDB("UPDATE pets SET isSleeping = 0 WHERE id=" + petSQL[i].id);
+                    console.log("DB: Waking up pet");
+                    client.users.get(petSQL[i].ownerName).send("Your active pet **" + petSQL[i].name + "** has woken up!");
                 }
-                if (petSQL[i].isEgg == 1) {
-                    var hatchTime = petSQL[i].hatchTime;
-                    if (currentTime > hatchTime) {
-                        console.log("DB: Updated pet ID " + petSQL[i].id + " to egg status 0!");
-                        let petType = await Utils.queryDB("SELECT id, name, rarity FROM pet_types");
-                        var pets = new Array();
-                        var petsWeight = new Array();
-                        for (var pi = 0; pi < petType.length; pi++) {
-                            pets.push(petType[pi].id);
-                            petsWeight.push(petType[pi].rarity);
-                        }
-                        var totalweight = eval(petsWeight.join("+"));
-                        var weighedpets = new Array();
-                        var currPet = 0;
-                        while (currPet < pets.length) {
-                            for (var pw = 0; pw < petsWeight[currPet]; pw++)
-                                weighedpets[weighedpets.length] = pets[currPet];
-                            currPet++;
-                        }
-
-                        var randomnumber = Math.floor(Math.random() * totalweight);
-                        var rarity = Utils.petTypeString(petType[pi].rarity);
-						
-                        var randomPetID = weighedpets[randomnumber];
-                        var petName = petType[randomPetID - 1].name;
-
-                        let bgType = await Utils.queryDB("SELECT id, name FROM backgrounds");
-                        var randomBG = Math.floor(Math.random() * bgType.length);
-                        var bgName = bgType[randomBG].name;
-                        var bgID = bgType[randomBG].id;
-
-                        await Utils.queryDB("UPDATE pets SET petType=" + randomPetID + ", bgType=" + bgID + " WHERE id=" + petSQL[i].id);
-                        await Utils.queryDB("UPDATE pets SET isEgg=0 WHERE id=" + petSQL[i].id);
-                        console.log("DB: Hatching pet!");
-                        client.users.get(petSQL[i].ownerName).send("Your pet has hatched into a **"+petName+"** (**"+rarity+"**)!");
+            }
+            if (petSQL[i].isEgg == 1) {
+                var hatchTime = petSQL[i].hatchTime;
+                if (currentTime > hatchTime) {
+                    console.log("DB: Updated pet ID " + petSQL[i].id + " to egg status 0!");
+                    let petType = await Utils.queryDB("SELECT id, name, rarity FROM pet_types");
+                    var pets = new Array();
+                    var petsWeight = new Array();
+                    for (var pi = 0; pi < petType.length; pi++) {
+                        pets.push(petType[pi].id);
+                        petsWeight.push(petType[pi].rarity);
                     }
+                    var totalweight = eval(petsWeight.join("+"));
+                    var weighedpets = new Array();
+                    var currPet = 0;
+                    while (currPet < pets.length) {
+                        for (var pw = 0; pw < petsWeight[currPet]; pw++)
+                            weighedpets[weighedpets.length] = pets[currPet];
+                        currPet++;
+                    }
+
+                    var randomnumber = Math.floor(Math.random() * totalweight);
+                    var rarity = Utils.petTypeString(petType[pi].rarity);
+
+                    var randomPetID = weighedpets[randomnumber];
+                    var petName = petType[randomPetID - 1].name;
+
+                    let bgType = await Utils.queryDB("SELECT id, name FROM backgrounds");
+                    var randomBG = Math.floor(Math.random() * bgType.length);
+                    var bgName = bgType[randomBG].name;
+                    var bgID = bgType[randomBG].id;
+
+                    await Utils.queryDB("UPDATE pets SET petType=" + randomPetID + ", bgType=" + bgID + " WHERE id=" + petSQL[i].id);
+                    await Utils.queryDB("UPDATE pets SET isEgg=0 WHERE id=" + petSQL[i].id);
+                    console.log("DB: Hatching pet!");
+                    client.users.get(petSQL[i].ownerName).send("Your pet has hatched into a **" + petName + "** (**" + rarity + "**)!");
                 }
             }
         }
-		await petUpdates();
+    }
+    await petUpdates();
 }
 
 // -----------------------------------
@@ -125,32 +125,32 @@ async function petUpdates() {
 // -----------------------------------
 
 async function updateUser(member, message) {
-	console.log("DB: User ID is "+member);
-	let queryRes = await Utils.queryDB("SELECT * FROM users WHERE discordID="+member);
-	if(queryRes && queryRes.length) {
-		var userID = queryRes[0].id;
+    console.log("DB: User ID is " + member);
+    let queryRes = await Utils.queryDB("SELECT * FROM users WHERE discordID=" + member);
+    if (queryRes && queryRes.length) {
+        var userID = queryRes[0].id;
         var active = queryRes[0].activePet;
         let petRes = await Utils.queryDB("SELECT * FROM pets WHERE id=" + active);
         let userAch = await Utils.queryDB("SELECT achievements FROM users WHERE id=" + userID)
-		var postCount = queryRes[0].xp;
+        var postCount = queryRes[0].xp;
         var petLvl = petRes[0].level;
-		var foodStored = queryRes[0].food;
+        var foodStored = queryRes[0].food;
         var AchJSON = JSON.parse(userAch[0].achievements);
-		var petID = JSON.parse(queryRes[0].petID);
-		await Utils.queryDB("UPDATE users SET xp = xp + 1 WHERE discordID = "+member);
-		await Utils.queryDB("UPDATE server SET jackpotAmount = jackpotAmount + 1 WHERE id = 1");
-		console.log("DB: Added 1 XP to "+member);
-		var urCount = 0;
-		var rCount = 0;
-		for(var pi = 0; pi < petID.length; pi++) {
-			let newPetRes = await Utils.queryDB("SELECT * FROM pets WHERE id=" + petID[pi]);
-			var type = newPetRes[0].petType;
-			let petTypeRes = await Utils.queryDB("SELECT * FROM pet_types WHERE id="+type);
-			var rarity = petTypeRes[0].rarity;
-			if(rarity == 1) urCount++;
-			if(rarity == 3) rCount++;
-		}
-		console.log("DB: Found "+urCount+" ultra rares!");
+        var petID = JSON.parse(queryRes[0].petID);
+        await Utils.queryDB("UPDATE users SET xp = xp + 1 WHERE discordID = " + member);
+        await Utils.queryDB("UPDATE server SET jackpotAmount = jackpotAmount + 1 WHERE id = 1");
+        console.log("DB: Added 1 XP to " + member);
+        var urCount = 0;
+        var rCount = 0;
+        for (var pi = 0; pi < petID.length; pi++) {
+            let newPetRes = await Utils.queryDB("SELECT * FROM pets WHERE id=" + petID[pi]);
+            var type = newPetRes[0].petType;
+            let petTypeRes = await Utils.queryDB("SELECT * FROM pet_types WHERE id=" + type);
+            var rarity = petTypeRes[0].rarity;
+            if (rarity == 1) urCount++;
+            if (rarity == 3) rCount++;
+        }
+        console.log("DB: Found " + urCount + " ultra rares!");
         var equipmentList = JSON.parse(queryRes[0].equipmentList);
         const itemRes = await Utils.queryDB("SELECT * FROM items");
         console.log("DB: Selected pet ID " + active);
@@ -175,12 +175,12 @@ async function updateUser(member, message) {
                 }
             }
         }
-		
-		var equipCount = hasHG+hasTR+hasPW+hasMI;
-		
-		await Utils.queryDB("UPDATE achievement_progress SET hgNumber="+hasHG+", trNumber="+hasTR+", pwNumber="+hasPW+", petLevel="+petLvl+", ultraRareOwned="+urCount+", rareOwned="+rCount+", postCount="+postCount+", achUnlocked="+AchJSON.length+", foodStored="+foodStored+", equipCount="+equipCount+" WHERE id="+userID);
-		
-		console.log("DB: Updated achievement_progress for user ID "+userID);
+
+        var equipCount = hasHG + hasTR + hasPW + hasMI;
+
+        await Utils.queryDB("UPDATE achievement_progress SET hgNumber=" + hasHG + ", trNumber=" + hasTR + ", pwNumber=" + hasPW + ", petLevel=" + petLvl + ", ultraRareOwned=" + urCount + ", rareOwned=" + rCount + ", postCount=" + postCount + ", achUnlocked=" + AchJSON.length + ", foodStored=" + foodStored + ", equipCount=" + equipCount + " WHERE id=" + userID);
+
+        console.log("DB: Updated achievement_progress for user ID " + userID);
 
         let achProgRes = await Utils.queryDB("SELECT * FROM achievement_progress WHERE id=" + userID)
         var achKeys = Object.keys(achProgRes[0]);
@@ -206,13 +206,33 @@ async function updateUser(member, message) {
                 }
             }
         }
-	} else {
-		console.log("DB: "+member+" was NOT found, adding to database!");	
-		let addMemberRes = await Utils.queryDB("INSERT INTO users(discordID) VALUES ("+member+"); ");
-		console.log("DB: "+member+" successfully added to database as ID "+addMemberRes.insertId+"!");
-		let addAchievementQuery = Utils.queryDB("INSERT INTO achievement_progress(id) VALUES ("+addMemberRes.insertId+")");
-		console.log("DB: Successfully added to achievement_progress!");
-	}
+    } else {
+        console.log("DB: " + member + " was NOT found, adding to database!");
+        let addMemberRes = await Utils.queryDB("INSERT INTO users(discordID) VALUES (" + member + "); ");
+        console.log("DB: " + member + " successfully added to database as ID " + addMemberRes.insertId + "!");
+        let addAchievementQuery = Utils.queryDB("INSERT INTO achievement_progress(id) VALUES (" + addMemberRes.insertId + ")");
+        console.log("DB: Successfully added to achievement_progress!");
+    }
+}
+
+// --------------------------
+// Let's deal with the guests
+// --------------------------
+
+async function kickGuests() {
+	const list = client.guilds.get("456965754827571202");
+	var currentTime = Utils.getTimestamp;
+	var kickNum = 0;
+	list.members.forEach(async member => {
+		let hasGuestRole = member.roles.some(role => role.name == "Guests");
+		let joinTime = member.joinedTimestamp + 172800;
+		if(joinTime > currentTime) {
+			await member.kick();
+			client.channels.get('495610171759001621').send(member.displayName+" has been kicked for being inactive in the entrance for longer than 2 days.");
+			kickNum++;
+		}
+	});
+	return kickNum; 
 }
 
 // -------------------------------
@@ -220,15 +240,15 @@ async function updateUser(member, message) {
 // -------------------------------
 
 client.on('message', async message => {
-	if(message.author.bot) return;
-	await updateUser(message.author.id, message);
+    if (message.author.bot) return;
+    await updateUser(message.author.id, message);
 });
 
 client.on('ready', () => {
-	console.log("Bot has started, with "+client.users.size+" users!"); 
-	client.user.setActivity("with dragon butts");
-	newDay();
-	petUpdates();
+    console.log("Bot has started, with " + client.users.size + " users!");
+    client.user.setActivity("with dragon butts");
+    newDay();
+    petUpdates();
 });
 
 client.login(config.token);
