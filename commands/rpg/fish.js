158cd32c61f3c83811be523a5fd59ca48c7c063b
Xenoyia <ccplukeg@gmail.com>
2019-02-11 00:12:13 +0000

    Update fish.js

--- commands/rpg/fish.js
+++ commands/rpg/fish.js
@@ -1,11 +1,6 @@
 const { Command } = require('discord.js-commando');
 const Utils = require('../../core/utils.js');
 const Discord = require('discord.js');
-const times = {
-	'1': 3000,
-	'2': 2000,
-	'3': 1000
-};
 
 module.exports = class FishCommand extends Command {
 	constructor(client) {
@@ -15,57 +10,41 @@ module.exports = class FishCommand extends Command {
 			memberName: 'fish',
 			description: 'Go fishing.'
 		});
+		
+		this.fishing = new Map();
 	}
 	
 	async run(msg) {
 		if(await Utils.isInBattle(msg.author)) {
 			return msg.say("You're in a battle, finish that before using this command!");
 		}
+		if (this.fishing.has(msg.author.id)) return msg.reply('You are already fishing!');
 		const embedMsg = new Discord.RichEmbed()
 			.setAuthor("World of the House of Dragons", "https://i.imgur.com/CyAb3mV.png")
 			
 		if(await Utils.canUseAction(msg.author, 'fish')) {
-			let fishing = true;
-			embedMsg.addField("Fishing", "You are now fishing. Type `reel` as soon as something bites, and `stop` to stop fishing!");
+			this.fishing.set(msg.author.id, true);
+			embedMsg.addField("Fishing", "You are now fishing. Type `reel` as soon as something bites!");
 			await msg.embed(embedMsg);
-			while(fishing) {
-				let stopFishing = false;
-				let timeUntilFish = Utils.randomIntIn(2000,30000);
-				setTimeout(stopFishing = await this.awaitStop(msg, timeUntilFish), 0);
-				if(stopFishing) {
-					fishing = false;
-					break;
-				}
-				setTimeout(await this.fishBite(msg), timeUntilFish);
+			let stopFishing = false;
+			let timeUntilFish = Utils.randomIntIn(3000,30000);
+			let fishTime = Utils.randomIntIn(1000,3000);
+			console.log(timeUntilFish);
+			await Utils.delay(timeUntilFish);
+			await msg.embed(Utils.makeRPGEmbed("Something's Biting!", "Type **reel** quickly to catch it!"));
+			let msgs = await msg.channel.awaitMessages(res => res.author.id === msg.author.id, {
+				max: 1,
+				time: fishTime
+			});
+			if (!msgs.size || msgs.first().content !== 'reel') {
+				await msg.embed(Utils.makeRPGEmbed("Got Away!", "Whatever was biting got away!"));
+			} else {
+				await msg.reply('You caught a fish!');
 			}
+			this.fishing.delete(msg.author.id);
 		} else {
 			embedMsg.addField("Can't Fish", "There's nowhere to fish here, find a lake or ocean on the `!map`!");
 			return msg.embed(embedMsg);
 		}
-	}
-	
-	async fishBite(msg) {
-		await msg.embed(Utils.makeRPGEmbed("Something's Biting!", "Type **reel** quickly to catch it!"));
-		let randomTime = Utils.randomIntEx(0, times.length);
-		const time = times[randomTime];
-		const msgs = await msg.channel.awaitMessages(res => res.author.id === msg.author.id, {
-			max: 1,
-			time: time
-		});
-		if (!msgs.size || msgs.first().content !== 'reel') return msg.reply('The fish got away!');
-		// Do fish logic here!
-		return msg.reply('You caught a fish!');
-	};
-	
-	async awaitStop(msg, time) {
-		const msgs = await msg.channel.awaitMessages(res => res.author.id === msg.author.id, {
-			max: 1,
-			time: time
-		});
-		if (msgs.content && msgs.first().content == 'stop') {
-			await msg.embed(Utils.makeRPGEmbed("Stopped Fishing", "You stopped fishing!"));
-			return true;
-		}
-		else return false;
 	}
 }
