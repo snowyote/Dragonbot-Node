dda6d877bb5bc51420251c2d2b266fba341d969a
Xenoyia <ccplukeg@gmail.com>
2019-02-13 11:17:04 +0000

    Update equipment.js

--- data/equipment.js
+++ data/equipment.js
@@ -283,86 +283,102 @@ const equipment = {
                 agility: -2
             }
         ],
-		
+
         helm = [
             cap = {
                 name: 'Cap',
                 type: 'helm',
+                rarity: 100,
                 fortitude: 0.25
             },
             skull_cap = {
                 name: 'Skull Cap',
                 type: 'helm',
+                rarity: 100,
                 fortitude: 0.5
             },
             helm = {
                 name: 'Helm',
                 type: 'helm',
+                rarity: 90,
                 fortitude: 0.75
             },
             full_helm = {
                 name: 'Full Helm',
                 type: 'helm',
+                rarity: 80,
                 fortitude: 1
             },
             demon_mask = {
                 name: 'Demon Mask',
                 type: 'helm',
+                rarity: 70,
                 fortitude: 1.25
             },
             horned_helm = {
                 name: 'Horned Helm',
                 type: 'helm',
+                rarity: 60,
                 fortitude: 1.5
             },
             great_helm = {
                 name: 'Great Helm',
                 type: 'helm',
+                rarity: 50,
                 fortitude: 1.75
             },
             crown = {
                 name: 'Crown',
                 type: 'helm',
+                rarity: 30,
                 fortitude: 2
             },
             war_hat = {
                 name: 'War Hat',
                 type: 'helm',
+                rarity: 20,
                 fortitude: 2.25
             },
             sallet = {
                 name: 'Sallet',
                 type: 'helm',
+                rarity: 15,
                 fortitude: 2.5
             },
             casque = {
                 name: 'Casque',
                 type: 'helm',
+                rarity: 10,
                 fortitude: 2.75
             },
             basinet = {
                 name: 'Basinet',
                 type: 'helm',
+                rarity: 5,
                 fortitude: 3
             },
             winged_helm = {
                 name: 'Winged Helm',
                 type: 'helm',
+                rarity: 4,
                 fortitude: 3.25
             },
             grand_crown = {
                 name: 'Grand Crown',
                 type: 'helm',
+                rarity: 3,
                 fortitude: 3.5
             },
             spired_helm = {
                 name: 'Spired Helm',
                 type: 'helm',
+                rarity: 2,
                 fortitude: 3.75
             },
             accursed_visage = {
                 name: 'Accursed Visage',
                 type: 'helm',
+                rarity: 1,
                 fortitude: 4
             }
         ],
@@ -463,105 +479,115 @@ const equipment = {
                 rarity: 1
             }
         ],
-		
-		legs = [
-            shirt = {
-                name: 'Shirt',
-                type: 'chest',
-                fortitude: 0,
-                rarity: 100,
-                agility: 4
-            },
-            padded_chest = {
-                name: 'Padded Chest',
-                type: 'chest',
+
+        legs = [
+            pants = {
+                name: 'Pants',
+                type: 'legs',
                 fortitude: 0.25,
-                rarity: 90,
+                rarity: 100,
                 agility: 3
             },
-            studded_chest = {
-                name: 'Studded Chest',
-                type: 'chest',
+            leggings = {
+                name: 'Leggings',
+                type: 'legs',
                 fortitude: 0.5,
                 rarity: 80,
                 agility: 2
             },
-            ring_mail = {
-                name: 'Ring Mail',
-                type: 'chest',
+            breeches = {
+                name: 'Breeches',
+                type: 'legs',
                 fortitude: 0.75,
-                rarity: 70,
+                rarity: 60,
                 agility: 1
             },
-            scale_mail = {
-                name: 'Scale Mail',
-                type: 'chest',
-                fortitude: 1,
-                rarity: 60
+            faulds = {
+                name: 'Faulds',
+                type: 'legs',
+                rarity: 40,
+                fortitude: 1
             },
-            chainmail = {
-                name: 'Chainmail',
-                type: 'chest',
+            plate_leggings = {
+                name: 'Plate Leggings',
+                type: 'legs',
                 fortitude: 1.25,
-                rarity: 50
+                rarity: 20,
+                agility: -1
             },
-            light_plate = {
-                name: 'Light Plate',
-                type: 'chest',
+            tassets = {
+                name: 'Tassets',
+                type: 'legs',
                 fortitude: 1.5,
-                rarity: 30
+                rarity: 15,
+                agility: -2
             },
-            heavy_plate = {
-                name: 'Heavy Plate',
-                type: 'chest',
+            cuisses = {
+                name: 'Cuisses',
+                type: 'legs',
                 fortitude: 1.75,
-                rarity: 25
-            },
-            ornate_chest = {
-                name: 'Ornate chest',
-                type: 'chest',
-                fortitude: 2,
-                rarity: 20
-            },
-            chaos_plate = {
-                name: 'Chaos Plate',
-                type: 'chest',
-                fortitude: 2.25,
-                rarity: 15
-            },
-            ancient_chest = {
-                name: 'Ancient chest',
-                type: 'chest',
-                fortitude: 2.5,
-                rarity: 10
-            },
-            archon_plate = {
-                name: 'Archon Plate',
-                type: 'chest',
-                fortitude: 2.75,
-                rarity: 5
-            },
-            sacred_plate = {
-                name: 'Sacred Plate',
-                type: 'chest',
-                fortitude: 3,
-                rarity: 3
-            },
-            kraken_shell = {
-                name: 'Kraken Shell',
-                type: 'chest',
-                fortitude: 3.5,
-                rarity: 2
-            },
-            shadow_plate = {
-                name: 'Shadow Plate',
-                type: 'chest',
-                fortitude: 4,
-                rarity: 1
+                rarity: 10,
+                agility: -3
             }
         ],
-		
-		
+
+        boots = [
+            shoes = {
+                name: 'Shoes',
+                type: 'boots',
+                fortitude: 0.25,
+                rarity: 100,
+                agility: 3
+            },
+            boots = {
+                name: 'Boots',
+                type: 'boots',
+                fortitude: 0.5,
+                rarity: 80,
+                agility: 2
+            },
+            chain_boots = {
+                name: 'Chain Boots',
+                type: 'boots',
+                fortitude: 0.75,
+                rarity: 60,
+                agility: 1
+            },
+            light_plated_boots = {
+                name: 'Light Plated Boots',
+                type: 'boots',
+                rarity: 40,
+                fortitude: 1
+            },
+            heavy_plated_boots = {
+                name: 'Heavy Plated Boots',
+                type: 'boots',
+                fortitude: 1.25,
+                rarity: 20,
+                agility: -1
+            },
+            greaves = {
+                name: 'Greaves',
+                type: 'boots',
+                fortitude: 1.5,
+                rarity: 15,
+                agility: -2
+            },
+            war_boots = {
+                name: 'War Boots',
+                type: 'boots',
+                fortitude: 1.75,
+                rarity: 10,
+                agility: -3
+            },
+            mirrored_boots = {
+                name: 'Mirrored Boots',
+                type: 'boots',
+                rarity: 5,
+                fortitude: 2,
+                agility: -4
+            }
+        ]
     ]
 };
 
